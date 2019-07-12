ce99de11c8b8981e26eee91caf9268ecb6a91192
Scott Donaldson <scott@isl.co>
2019-07-12 16:16:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+const urls = fs.readFileSync("./urls.txt", "utf-8").split("\n");
+
+urls.forEach(url => {
+  axios(url).then(res => {
+    console.log(url);
+    const { document } = new JSDOM(res.data).window;
+    const title = document.querySelector("h1").textContent;
+    const headings = Array.from(document.querySelectorAll("h2, h3"));
+    for (let i = 0; i < headings.length; i++) {
+      const h = headings[i];
+      if (!h.textContent.toLowerCase().includes("death")) continue;
+
+      let output = title + "\n\n";
+      let current = h.nextSibling;
+      while (current.tagName !== "H2" && current.tagName !== "H3") {
+        output += current.textContent + "\n";
+        current = current.nextSibling;
+      }
+      output = output.replace(/\[\d*\]/g, "");
+      output = output.trim();
+      console.log(output);
+      fs.writeFileSync(
+        "./obits/" + title.toLowerCase().replace(/[^a-z]/g, "") + ".txt",
+        output,
+        "utf-8"
+      );
+      break;
+    }
+  });
+});
